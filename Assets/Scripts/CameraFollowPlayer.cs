29c3908b0e8ad674450258ba4655cfa9c07680c5
Mel Rodrigues <melrodriguesgames@gmail.com>
2021-01-30 16:58:16 -0800

    Camera follow player script.

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    private Transform playerTransform;
+    [SerializeField] private Vector3 offset;
+
+    private void Awake()
+    {
+        playerTransform = FindObjectOfType<PlayerController>().gameObject.transform;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = playerTransform.position + offset;
+    }
+}
